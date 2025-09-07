baf0e3a4800de3158c10f97cc493a6759a574d0b
Anand Prakash Srivastava <srivastavaanandprakash16@gmail.com>
2025-09-07 22:46:51 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,6 @@ app.use("/api/stories", routes);
 
 app.get("/", (req, res) => {
     res.send("Story Generator API is running");
-}
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
